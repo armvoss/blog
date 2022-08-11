a6521569499e41b3eccc522441d42004822fcf47
armvoss <voss@ggb.de>
2022-08-11 07:40:13 +0200

    delete_html added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 # blogs/urls.py
 from django.urls import path, include
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
-    path("", BlogListView.as_view(), name="home")
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("", BlogListView.as_view(), name="home"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/delete", BlogDeleteView.as_view(), name="post_delete")
 ]
